05df5e90110081a88bd4ae02b9d5d98e8b2c3a83
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-02-24 09:40:37 +0000

    002: Implemented expected dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -56,4 +57,22 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // RoomDB Additions
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Compose Testing
+    debugImplementation(libs.ui.test.manifest)
+
+    // Glance Widget
+    implementation(libs.androidx.glance.material3)
+    implementation(libs.androidx.glance)
+    implementation(libs.androidx.glance.preview)
+    implementation(libs.androidx.glance.appwidget.preview)
+    implementation(libs.androidx.glance.appwidget)
+
+    // Coroutines Testing
+    testImplementation(libs.kotlinx.coroutines.test)
 }
