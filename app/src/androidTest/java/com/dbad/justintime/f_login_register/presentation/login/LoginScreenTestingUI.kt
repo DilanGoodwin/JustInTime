2cfadbaf3664e227b0570ab05310970d8624264e
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-02-24 15:10:15 +0000

    loginRegisterScreens 002: test - implemented LoginScreenTestingUI checkValuesDisplayed tests

--- app/src/androidTest/java/com/dbad/justintime/f_login_register/presentation/login/LoginScreenTestingUI.kt
+++ app/src/androidTest/java/com/dbad/justintime/f_login_register/presentation/login/LoginScreenTestingUI.kt
@@ -1,6 +1,10 @@
 package com.dbad.justintime.f_login_register.presentation.login
 
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.dbad.justintime.R
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
@@ -9,15 +13,26 @@ import org.junit.Test
 class LoginScreenTestingUI {
 
     @get:Rule
-    val testRule = createComposeRule()
+    val testRule = createAndroidComposeRule<ComponentActivity>()
 
     @Before
     fun reset() = runTest {
         testRule.setContent { LoginScreen() }
     }
+    
+    private fun checkValuesExist(screenValue: String) {
+        testRule.onNodeWithText(text = screenValue)
+            .assertExists(errorMessageOnFail = "Login Screen component '$screenValue' does not exist")
+            .assertIsDisplayed()
+    }
 
     @Test
-    fun checkValuesDisplayed() = runTest {}
+    fun checkValuesDisplayed() = runTest {
+        checkValuesExist(screenValue = testRule.activity.getString(R.string.email))
+        checkValuesExist(screenValue = testRule.activity.getString(R.string.password))
+        checkValuesExist(screenValue = testRule.activity.getString(R.string.login))
+        checkValuesExist(screenValue = testRule.activity.getString(R.string.register))
+    }
 
     @Test
     fun checkEmailFieldErrors() = runTest {}
