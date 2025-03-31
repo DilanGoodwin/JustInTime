700a09c6c70bac2995172723ce81d5d2ad2cf903
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-03-31 12:03:30 +0100

    shifts 004: #31 - Compact Single Shift view UI

---
+++ app/src/main/java/com/dbad/justintime/f_shifts/presentation/shifts_list/ShiftViewList.kt
@@ -0,0 +1,75 @@
+package com.dbad.justintime.f_shifts.presentation.shifts_list
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.dbad.justintime.core.presentation.util.LabelledTextInputFields
+import com.dbad.justintime.core.presentation.util.ViewingSystemThemes
+import com.dbad.justintime.ui.theme.JustInTimeTheme
+
+// Stateless
+@Composable
+fun IndividualShift() {
+    Box(
+        modifier = Modifier
+            .padding(all = 10.dp)
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.LightGray, shape = RoundedCornerShape(size = 5.dp))
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(space = 20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 5.dp)
+        ) {
+            Column(verticalArrangement = Arrangement.spacedBy(space = 10.dp)) {
+                LabelledTextInputFields(
+                    currentValue = "",
+                    placeHolderText = "Location",
+                    onValueChange = {},
+                    readOnly = true,
+                    modifier = Modifier.fillMaxWidth(fraction = 0.5f)
+                )
+                LabelledTextInputFields(
+                    currentValue = "",
+                    placeHolderText = "Role",
+                    onValueChange = {},
+                    readOnly = true,
+                    modifier = Modifier.fillMaxWidth(fraction = 0.5f)
+                )
+            }
+
+            Column(verticalArrangement = Arrangement.spacedBy(space = 10.dp)) {
+                LabelledTextInputFields(
+                    currentValue = "",
+                    placeHolderText = "Date",
+                    onValueChange = {},
+                    readOnly = true,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                LabelledTextInputFields(
+                    currentValue = "",
+                    placeHolderText = "Time",
+                    onValueChange = {},
+                    readOnly = true,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@ViewingSystemThemes
+@Composable
+fun PreviewIndividualShift() {
+    JustInTimeTheme { IndividualShift() }
+}
